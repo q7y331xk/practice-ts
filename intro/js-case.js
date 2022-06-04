28c6229fbe43550a97f93f4b42303ffedd2ad263
dulee <dulee.dev@gmail.com>
2022-06-04 23:35:08 +0900

    intro/

---
+++ intro/js-case.js
@@ -0,0 +1,4 @@
+const array = [1,2,3,4];
+const bool = true;
+
+console.log(array + bool);
---
+++ intro/ts-case.ts
@@ -0,0 +1,5 @@
+const array1 = [1,2,3,4];
+const bool1 = true;
+
+console.log(array1 + bool1);
+// make error
