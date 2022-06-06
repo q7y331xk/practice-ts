0931b36ee9997fc8f3903a9d26c27a37cf80b2f1
dulee <dulee.dev@gmail.com>
2022-06-06 23:42:07 +0900

    ..;

--- class-orient/classes.ts
+++ class-orient/classes.ts
@@ -1,5 +1,4 @@
 'use strict';
-
 class Player {
     constructor(
         private firstName: string,
@@ -9,5 +8,40 @@ class Player {
 }
 
 const dulee = new Player("lee","du","dulee")
-
 console.log(dulee.nickName);
+
+console.log("dictionary example");
+
+type Words = {
+    [key: string]: string
+}
+
+class Dict {
+    private words: Words
+    constructor() {
+        this.words = {}
+    }
+
+    add(word: Word) {
+        if (this.words[word.term] === undefined) {
+            this.words[word.term] = word.def;
+        }
+    }
+
+    def(term: string) {
+        return this.words[term]
+    }
+}
+
+class Word {
+    constructor(
+        public term: string,
+        public def: string
+    ) {}
+}
+
+const kimchi = new Word("kimchi", "korean food")
+
+const dict = new Dict()
+dict.add(kimchi)
+dict.def("korean food")
